9495fdfea8fc8e7b4e8be5e7f2f569916725e0f3
Ali Ahmed <ali@gmail.com>
2024-12-18 10:19:40 +0500

    Database Connection and created the todo Model

--- app/api/route.js
+++ app/api/route.js
@@ -1,7 +1,29 @@
+import { connectDB } from "@/lib/config/db";
+import TodoModel from "@/lib/models/todoModel";
 import { NextResponse } from "next/server";
+
+const loadDb = async () => {
+    await connectDB();
+}
+
+loadDb();
 
 export async function GET(request){
     return NextResponse.json({
         message : "GET Method Hit"
+    });
+}
+
+export async function POST(request){
+
+    const {title, description} = await request.json();
+
+    await TodoModel.create({
+        title,
+        description
+    });
+
+    return NextResponse.json({
+        message : "Todo Created"
     });
 }
